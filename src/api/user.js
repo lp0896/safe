59591a0c6427d816a3b72d0267a8b565d872643b
lipeng357 <lipeng357@jd.com>
2019-07-21 12:13:13 +0800

    提交项目架构

---
+++ src/api/user.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function login() {
+  return request({
+    url: '/login',
+    method: 'get'
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'get'
+  })
+}
---
+++ src/views/homepage/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard-container">这里是首页
+    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
+  </div>
+</template>
+
+<script>
+// import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Homepage',
+  computed: {
+    // ...mapGetters([
+    //   'name'
+    // ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
